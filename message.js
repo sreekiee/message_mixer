70e59e1f631cab68c864425fc20fc2a3c802f10d
Sreekar Reddy <sreekar6@gmail.com>
2018-01-18 12:44:37 +0530

    Add files via upload
    Use exports and imports - 2

--- message.js
+++ message.js
@@ -1,13 +1,14 @@
-import MessageMixer from './messageMixer';
+import { countCharacter, capitalizeFirstCharacterOfWords, reverseWord, reverseAllWords, replaceFirstOccurence, replaceAllOccurrences, encode, palindrome, pigLatin} from './messageMixer';
+
 function displayMessage() {
-  console.log(MessageMixer.countCharacter("What is the color of the sky?", "t"));
-  console.log(MessageMixer.capitalizeFirstCharacterOfWords("What is the color of the sky?"));
-  console.log(MessageMixer.reverseWord("What is the color of the sky?"));
-  console.log(MessageMixer.reverseAllWords("What is the color of the sky?"));
-  console.log(MessageMixer.replaceFirstOccurence("What is the color of the sky?", "sky", "water"));
-  console.log(MessageMixer.encode("What is the color of the sky?"));
-  console.log(MessageMixer.palindrome('Wassup'));
-  console.log(MessageMixer.pigLatin('holla so you are my', 'bro'));
+  console.log(countCharacter("What is the color of the sky?", "t"));
+  console.log(capitalizeFirstCharacterOfWords("What is the color of the sky?"));
+  console.log(reverseWord("What is the color of the sky?"));
+  console.log(reverseAllWords("What is the color of the sky?"));
+  console.log(replaceFirstOccurence("What is the color of the sky?", "sky", "water"));
+  console.log(encode("What is the color of the sky?"));
+  console.log(palindrome('Wassup'));
+  console.log(pigLatin('holla so you are my', 'bro'));
 }
 
 displayMessage();
--- message_mixer.js
+++ message_mixer.js
@@ -1,6 +1,4 @@
-let MessageMixer = {};
-
-MessageMixer.countCharacter = function(inputString, inputCharacter) {
+function countCharacter(inputString, inputCharacter) {
   let count = 0;
   let string = inputString.toLowerCase();
   let character = inputCharacter.toLowerCase();
@@ -12,7 +10,7 @@ MessageMixer.countCharacter = function(inputString, inputCharacter) {
   return count;
 };
 
-MessageMixer.capitalizeFirstCharacterOfWords = function(string) {
+function capitalizeFirstCharacterOfWords(string) {
   let arr = string.split(" ");
     for (let i = 0; i < arr.length; i++) {
       let word = arr[i];
@@ -22,42 +20,42 @@ MessageMixer.capitalizeFirstCharacterOfWords = function(string) {
 };
 
 
-MessageMixer.reverseWord = function(word) {
+function reverseWord(word) {
   return word.split("").reverse().join("");
 };
 
-MessageMixer.reverseAllWords = function(sentence) {
+function reverseAllWords(sentence) {
   let words = sentence.split(" ");
     for (let i = 0; i < words.length; i++) {
-      words[i] = MessageMixer.reverseWord(words[i]);
+      words[i] = reverseWord(words[i]);
     }
    return words.join(" ");
 };
 
 
-MessageMixer.replaceFirstOccurence = function(string, toBeReplaced, replaceWith) {
+function replaceFirstOccurence(string, toBeReplaced, replaceWith) {
   return string.replace(toBeReplaced, replaceWith);
 };
 
 
-MessageMixer.replaceAllOccurrences = function(string, toBeReplaced, replaceWith) {
+function replaceAllOccurrences(string, toBeReplaced, replaceWith) {
   return string.split(toBeReplaced).join(replaceWith);
 };
 
-MessageMixer.encode = function(string) {
+function encode(string) {
   let replacementObject = { "a": "@", "s": "$", "i": "!", "o":"0" };
     for (let key in replacementObject) {
-      string = MessageMixer.replaceAllOccurrences(string, key, replacementObject[key]);
+      string = replaceAllOccurrences(string, key, replacementObject[key]);
     }
     return string;
 };
 
-MessageMixer.palindrome = function(word){
-  return word + " " + MessageMixer.reverseWord(word);
+function palindrome(word){
+  return word + " " + reverseWord(word);
 };
 
-MessageMixer.pigLatin = function(sentence, character) {
+function pigLatin(sentence, character) {
   return sentence.split(" ").join(character + " ");
 };
 
-export default MessageMixer;
+export { countCharacter, capitalizeFirstCharacterOfWords, reverseWord, reverseAllWords, replaceFirstOccurence, replaceAllOccurrences, encode, palindrome, pigLatin };
